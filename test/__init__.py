c571ff9bfd109df8ae949461a3815de1907e3f09
Hào Nguyễn <fierolimb@gmail.com>
2022-12-26 21:21:24 +0900

    add _init_

--- test/__init__.py
+++ test/__init__.py
@@ -1,20 +1,13 @@
 import firebase_admin
-from firebase_admin import firestore
 from firebase_admin import credentials, db
+import json
 
-# Initialize the Firebase app with a service account, granting admin privileges
-cred = credentials.Certificate('C:/Users/fiero/Downloads/enji-9fe2b-firebase-adminsdk-npe4q-94d5acf441.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://enji-9fe2b-default-rtdb.firebaseio.com/'
-})
-
-# As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('/chat')
-
-print(firestore.SERVER_TIMESTAMP)
-# Add a new message to the database
-ref.push({
-    'sender': 'Alice',
-    'timestamp': firestore.Timestamp.now(),
-    'message': 'Hello, World!'
-})
+#Initialize creds and database link
+def initialize():
+    with open("creds_path.json", "r") as f :
+        cred = json.load(f)
+        cred = cred["creds_path"]
+    cred = credentials.Certificate(cred)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://enji-9fe2b-default-rtdb.firebaseio.com/'
+    })
